6f0f8d73bdc91ba607b987915546c2d3aeca7786
Karl Johan Krantz <schwomp@gmail.com>
2013-09-05 20:36:26 +0200

    : was the thing.

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -2,6 +2,7 @@ class user::dotfiles{
   $dotfiles_src = "https://github.com/${github_user}/dotfiles.git"
   $dotfiles_dir = "${user::homefolder}/src/dotfiles"
   vcsrepo{ $dotfiles_dir:
+    ensure => latest,
     provider => git,
     source => $dotfiles_src,
     revision => master,
