40d1844bb6871767098a4970cea509d05be6f70e
Karl Johan Krantz <schwomp@gmail.com>
2013-09-05 18:54:13 +0200

    :

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -1,7 +1,7 @@
 class user::dotfiles{
   $dotfiles_src = "https://github.com/${github_user}/dotfiles.git"
   $dotfiles_dir = "${user::homefolder}/src/dotfiles"
-  vcsrepo{ $dotfiles_dir
+  vcsrepo{ $dotfiles_dir:
     ensure => latest,
     provider => git,
     source => $dotfiles_src,
--- manifests/emacs.pp
+++ manifests/emacs.pp
@@ -1,7 +1,7 @@
 class user::emacs{
   $emacs_src = "https://github.com/${github_user}/emacs.d.git"
   $emacs_dir = "${user::homefolder}/src/emacs.d"
-  vcsrepo{ $emacs_dir
+  vcsrepo{ $emacs_dir:
     ensure => latest,
     provider => git,
     source => $emacs_src,
