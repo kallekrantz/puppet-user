c6205bee4d0129c09a9090b7fa9cabf4c2febcd8
Karl Johan Krantz <schwomp@gmail.com>
2013-09-05 18:55:54 +0200

    Ensure?

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -2,7 +2,6 @@ class user::dotfiles{
   $dotfiles_src = "https://github.com/${github_user}/dotfiles.git"
   $dotfiles_dir = "${user::homefolder}/src/dotfiles"
   vcsrepo{ $dotfiles_dir:
-    ensure => latest,
     provider => git,
     source => $dotfiles_src,
     revision => master,
--- manifests/emacs.pp
+++ manifests/emacs.pp
@@ -2,7 +2,6 @@ class user::emacs{
   $emacs_src = "https://github.com/${github_user}/emacs.d.git"
   $emacs_dir = "${user::homefolder}/src/emacs.d"
   vcsrepo{ $emacs_dir:
-    ensure => latest,
     provider => git,
     source => $emacs_src,
     revision => master,
