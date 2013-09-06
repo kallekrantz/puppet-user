8e4085f02f7334fe4b2a5a4ae43aa3fcaa1d414a
Karl Johan Krantz <schwomp@gmail.com>
2013-09-06 16:39:05 +0200

    Small fix aswel

--- manifests/shell.pp
+++ manifests/shell.pp
@@ -2,9 +2,10 @@ class user::shell{
   file{ "${user::homefolder}/bin":
     ensure => directory
   }
-  $command = "/bin/curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ${user::homefolder}/bin/vcprompt"
+  $command = "/usr/bin/curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ${user::homefolder}/bin/vcprompt"
   exec{$command:
     creates => "${user::homefolder}/bin/vcprompt",
+    require => Package[]
   }
   $chmodit = "/bin/chmod 755 ${user::homefolder}/bin/vcprompt"
   exec{$chmodit:
