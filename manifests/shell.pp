29494f0754b754b918ede5da6f40e4209c6f8ce8
Karl Johan Krantz <schwomp@gmail.com>
2013-09-06 16:35:54 +0200

    And need more qualification

--- manifests/shell.pp
+++ manifests/shell.pp
@@ -2,7 +2,7 @@ class user::shell{
   file{ "${user::homefolder}/bin":
     ensure => directory
   }
-  $command = "curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ${user::homefolder}/bin/vcprompt"
+  $command = "/bin/curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ${user::homefolder}/bin/vcprompt"
   exec{$command:
     creates => "${user::homefolder}/bin/vcprompt",
   }
